6607f4d5a6bfc0ab225b5bd46f6fc777a10b2a15
Gonzalo Marcos Vaquero <gmarcosvaquero@gmail.com>
2022-07-30 20:20:11 +0200

    Goofy aah sound

---
+++ UnityGame/Assets/Character/PlayerSounds.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Character;
+
+public class PlayerSounds : MonoBehaviour
+{
+    [SerializeField] private AudioSource audio;
+    [SerializeField] private AudioClip[] goofySounds;
+
+    void Update()
+    {
+        if(Input.GetKeyDown("e") || Input.GetKeyDown("space") || Input.GetKeyDown("q")) {
+            PlayRandomSound();
+        }
+
+        if(Input.GetKey("r")) { //chaos
+            PlayRandomSound();
+        }
+
+    }
+
+    void PlayRandomSound()
+    {         
+        int rand = Random.Range(0,goofySounds.Length);
+        Debug.Log(rand); 
+        audio.PlayOneShot(goofySounds[rand], 0.3f);
+    }
+}
